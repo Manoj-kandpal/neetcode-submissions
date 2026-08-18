61bb58e569891ed1ed3d77a23dd01bbf0477f34c
Manoj Kandpal <55502816+Manoj-kandpal@users.noreply.github.com>
2026-08-19 00:01:20 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = null;
+        if (list1 == null && list2 != null) {
+            return list2;
+        }
+        if (list2 == null && list1 != null) {
+            return list1;
+        }
+
+        if (list1 == null && list2 == null) return null;
+        ListNode current1 = list1;
+        ListNode current2 = list2;
+        ListNode prev = null;
+
+        while(current1 != null && current2 != null) {
+            while (current1 != null && current2 != null && current1.val <= current2.val) {
+                if (head == null) {
+                    head = current1;
+                    prev = current1;
+                } else {
+                    prev.next = current1;
+                    prev = prev.next;
+                }
+                current1 = current1.next;
+            }
+            while (current1 != null && current2 != null && current2.val < current1.val) {
+                if (head == null) {
+                    head = current2;
+                    prev = current2;
+                } else {
+                    prev.next = current2;
+                    prev = prev.next;
+                }
+                current2 = current2.next;
+            }
+        }
+
+        if (current1 == null && current2 != null) {
+            prev.next = current2;
+        } else if (current2 == null && current1 != null) {
+            prev.next = current1;
+        }
+
+        return head;
+    }
+}
