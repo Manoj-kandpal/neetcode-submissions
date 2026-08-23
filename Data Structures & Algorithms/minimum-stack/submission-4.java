c5c6efa2af876f967eb2d3d4112946f0b4f01240
Manoj Kandpal <55502816+Manoj-kandpal@users.noreply.github.com>
2026-08-24 00:45:17 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        this.stack.push(val);
+
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int popped = this.stack.pop();
+
+        if (minStack.peek() == popped) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return this.stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
